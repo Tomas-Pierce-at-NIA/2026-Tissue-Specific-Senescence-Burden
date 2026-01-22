f51d862792dca6fb69e272b16979b8abd0e89843
Tomas Pierce <piercetf@nih.gov>
2026-01-22 12:34:47 -0500

    starting project

---
+++ tissue_specific_senescence.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 22 12:22:40 2026
+
+@author: piercetf
+"""
+
+import pymc as pm
+from sklearn import model_selection as modsel
+import polars as pl
+
