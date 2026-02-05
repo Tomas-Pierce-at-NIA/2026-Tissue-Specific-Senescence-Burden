b3a40b6553435f2120a59d5d8eb93855a2529c19
Tomas Pierce <piercetf@nih.gov>
2026-02-05 14:25:21 -0500

    start on isolating data-loading concerns

---
+++ data.py
@@ -0,0 +1,5 @@
+
+SERUM = "data/Mouse Serum Samples (280 samples)_Protein_Group_Panel.tsv"
+MULTIORGAN = "data/Multiorgan_senescence_and_Olink.csv"
+SAMPLE_DESC = "data/Sample description for Seer.xlsx"
+
