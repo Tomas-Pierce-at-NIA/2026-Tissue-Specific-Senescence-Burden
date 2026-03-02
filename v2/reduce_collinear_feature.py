8d1ffaad2b5f4497b6010cd941b02892610537f5
Tomas Pierce <piercetf@nih.gov>
2026-03-02 18:41:51 -0500

    remember to add in reducing collinearity in own file

--- v2/reduce_collinear_feature.py
+++ v2/reduce_collinear_feature.py
@@ -3,17 +3,49 @@ from scipy.cluster import hierarchy
 from scipy.spatial import distance as spat_dist
 from scipy import stats
 import numpy as np
+import polars as pl
 
 def cluster_features(dtable):
-    transpose = dtable.to_numpy().T
+    transpose = np.array(dtable).T
     dist_mtx = spat_dist.pdist(transpose, 'correlation')
-    #sq_dist = sq_dist.squareform(dist_mtx)
     dist_link = hierarchy.centroid(dist_mtx)
     return dist_link
 
-
-    
-def get_cluster_root_indices(dist_link, max_clust=512):
+def get_cluster_rep_indices(dist_link, max_clust=512):
     flat = hierarchy.fcluster(dist_link, t=max_clust, criterion='maxclust')
-    leads = hierarchy.leaders(dist_link, flat)
+    cluster_ids, cluster_reps = np.unique(flat, return_index=True)
+    return cluster_reps
     
+
+class ClusterRepSel:
+    
+    def __init__(self, max_clust):
+        self.max_clust = max_clust
+        self.__n_clust = None
+        self.__cluster_reps = None
+    
+    def fit_transform(self, df):
+        clusters = cluster_features(df)
+        self.__cluster_reps = get_cluster_rep_indices(clusters, self.max_clust)
+        self.__n_clust = len(self.__cluster_reps)
+        sel_df = df[:, self.__cluster_reps]
+        return sel_df
+    
+    def fit(self, df):
+        self.fit_transform(df)
+    
+    def transform(self, df):
+        if self.__cluster_reps is None:
+            raise RuntimeError("tried to transform with unfitted cluster-rep selector")
+        else:
+            sel_df = df[:, self.__cluster_reps]
+            return sel_df
+    
+    def get_feature_names_out(self, feature_in):
+        if self.__cluster_reps is None:
+            raise RuntimeError("cluster-rep selector is not fitted")
+        else:
+            feat_name = np.array(feature_in)
+            feat_out = feat_name[self.__cluster_reps]
+            return feat_out
+
