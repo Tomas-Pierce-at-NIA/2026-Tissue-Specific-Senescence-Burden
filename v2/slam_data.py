2a1af759ed028419a4f0b16b00da7bf3138193ca
Tomas Pierce <piercetf@nih.gov>
2026-04-01 15:55:54 -0400

    start integrating SLAM data for further analysis

---
+++ v2/slam_data.py
@@ -0,0 +1,57 @@
+
+import polars as pl
+
+SLAM_EUTH = "data/slam900/Slam_900_phenotypes_combined w fast-fed.xlsx"
+SLAM_LONG_SERUM = "data/slam900/slam_900_counts_phenotypes.csv"
+
+
+def normalize_sample_id(table):
+    """normalize the sample ID code to contain no spaces so all are the same format"""
+    slamic_index = table.columns.index('SLAMICS ID')
+    table.replace_column(slamic_index, table['SLAMICS ID'].str.replace(' ', ''))
+
+
+def read_euth() -> pl.DataFrame:
+    """Reads in file which records which mice were euthanized and when from slam 900 dataset"""
+    table = pl.read_excel(
+        SLAM_EUTH,
+        schema_overrides={'Euthanasia Note': pl.String}
+    )
+    table = table.with_columns(
+        is_euthanized = pl.col('Euthanasia Note').is_not_null()
+    )
+    
+    # "gets euthanized" table
+    ge_tab = (table
+              .select(pl.col('ID'),pl.col('is_euthanized'))
+              .group_by(['ID'])
+              .sum()
+              .select(pl.col('ID'),pl.col('is_euthanized').alias('gets_euthanized'))
+    )
+    
+    table = table.join(ge_tab, how="inner", on="ID")
+    
+    table = table.select(pl.exclude(["Visit", "Euthanasia Note"]))
+    
+    normalize_sample_id(table)
+    
+    return table
+
+
+def read_serum():
+    """Reads in the longitudinal serum data taken from the slam 900 dataset"""
+    table = pl.read_csv(
+        SLAM_LONG_SERUM,
+        null_values = ["NA"],
+    )
+    normalize_sample_id(table)
+    return table
+
+
+def combine_serum_euth(serum_tab, euth_tab):
+    """Combine euthansia information with longitudinal serum data"""
+    # the way the serum identifies mice is not the way the euthanasia identifies mice
+    no_id_euth = euth_tab.select(pl.exclude('ID'))
+    return serum_tab.join(no_id_euth, how='left', on=['SLAMICS ID', 'Age', 'Sex', 'Strain'])
+
+
