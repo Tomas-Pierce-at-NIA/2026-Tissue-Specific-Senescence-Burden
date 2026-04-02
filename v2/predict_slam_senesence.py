c7734c1191dbf441f8bf877a69db724d6de7b1f5
Tomas Pierce <piercetf@nih.gov>
2026-04-02 17:17:29 -0400

    focus on features in common to SLAM data for attacking SLAM dynamics

--- v2/predict_slam_senesence.py
+++ v2/predict_slam_senesence.py
@@ -1,6 +1,23 @@
 
+import polars as pl
+
 import data
 from dataloader import DataLoader
 from data_prep import DataPrep
 from reduce_collinear_feature import ClusterRepSel
+import horseshoe
+import slam_data
 
+
+if __name__ == '__main__':
+    dl = DataLoader()
+    slam = slam_data.load_slam900_longitudinal()
+    pgmap = data.create_protein_gene_mapping()
+    long_pgmap = pgmap.filter(pl.col('Gene Names').is_in(pl.lit(slam.columns)))
+    
+    train_x = dl.get_train_predictors()
+    train_x2 = train_x.select(
+        [pl.col(c) for c in long_pgmap['Protein Names'].unique() if c in train_x.columns]
+    )
+    
+    
