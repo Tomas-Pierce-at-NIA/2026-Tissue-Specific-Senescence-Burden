39aaa6a9c5f68b6403f104094e65868af4714b1d
Tomas Pierce <piercetf@nih.gov>
2026-04-06 09:17:14 -0400

    keep stepping forward

--- v2/predict_slam_senesence.py
+++ v2/predict_slam_senesence.py
@@ -11,6 +11,7 @@ import slam_data
 
 if __name__ == '__main__':
     dl = DataLoader()
+    dprep = DataPrep()
     slam = slam_data.load_slam900_longitudinal()
     pgmap = data.create_protein_gene_mapping()
     long_pgmap = pgmap.filter(pl.col('Gene Names').is_in(pl.lit(slam.columns)))
@@ -19,5 +20,8 @@ if __name__ == '__main__':
     train_x2 = train_x.select(
         [pl.col(c) for c in long_pgmap['Protein Names'].unique() if c in train_x.columns]
     )
+    train_x3 = dprep.fit_transform(train_x2)
+    
+    
     
     
